feb31f4356e9bd37f0670c99ae0df3b4bb25cb5d
Nereg <47698366+Nereg@users.noreply.github.com>
2025-05-02 13:50:45 +0200

    feat: adds some basic SQL calls

---
+++ src/sql/queries/users.sql
@@ -0,0 +1,24 @@
+-- name: registerTelegram :exec
+WITH tmp_id AS (
+	INSERT INTO users (registred)
+	VALUES (timezone('utc', now()))
+	RETURNING id
+)
+INSERT INTO public.telegram(
+	"telegramId", "userId", "chatId")
+	SELECT sqlc.arg(tgUserId)::bigint,
+    id,
+    sqlc.arg(tgChatId)::bigint FROM tmp_id;
+;
+
+-- name: verifyUserByISIC :exec
+UPDATE users SET 
+    "ISICNum"=sqlc.arg(ISICNum)::bigint,
+    "facultyId"=sqlc.arg(facultyId)::int
+    WHERE "id"=$1;
+
+-- name: updatePermissions :exec
+INSERT INTO permissions ("userId", "permissions") 
+VALUES ($1, $2)
+ON CONFLICT ("userId") DO UPDATE 
+  SET permissions = excluded.permissions;
